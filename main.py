c59887ac0ee892ac477d354e8fe62e999a7b6e9d
Methaporn Limrostham <pedza507@gmail.com>
2026-08-25 22:42:33 +0700

    Fix port

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def home():
     return "✅ บอททำงานปกติ 24 ชม. แล้วจ้า!"
 
 def run_server():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run_server)
